945fec5218cdc588cda4a54f189c12329f92d462
KhangdaVinci <duongdangkhang53@gmail.com>
2026-03-13 23:00:05 +0700

    update: order-trigger/usecase

--- database/03_triggers/05_trg_order.sql
+++ database/03_triggers/05_trg_order.sql
@@ -1,4 +1,4 @@
--- Tự động cộng trừ tiền khi có thay đổi khi có thay đổi hóa đơn( Insert, update )
+-- Tự động cộng trừ tiền khi có thay đổi hóa đơn( Insert, update )
 CREATE OR REPLACE TRIGGER TRG_CTDONHANG_UPDATE
 AFTER INSERT OR UPDATE ON CTDONHANG
 FOR EACH ROW
@@ -12,6 +12,7 @@ BEGIN
         )
     WHERE MaDon = :NEW.MaDon;
 END;
+/
 
 -- Tự động cộng trừ tiền khi có thay đổi khi có thay đổi hóa đơn( delete )
 CREATE OR REPLACE TRIGGER TRG_CTDONHANG_DELETE
@@ -27,3 +28,4 @@ BEGIN
     )
     WHERE MaDon = :OLD.MaDon;
 END;
+/
