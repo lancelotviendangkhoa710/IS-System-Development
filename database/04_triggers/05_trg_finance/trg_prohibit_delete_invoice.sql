2ab2effeee8639464921dd28844c2af100f73d00
KhangdaVinci <duongdangkhang53@gmail.com>
2026-03-30 04:09:07 +0700

    update: remain lab

--- database/04_triggers/05_trg_finance/trg_prohibit_delete_invoice.sql
+++
@@ -1,8 +0,0 @@
--- Trigger cấm xóa dữ lệu hóa đơn
-CREATE OR REPLACE TRIGGER TRG_CAMXOAKETOAN_HOADON
-BEFORE DELETE ON HOADON
-FOR EACH ROW
-BEGIN
-    RAISE_APPLICATION_ERROR(PKG_ERROR_CODES.ERR_TC_CAM_XOA_HOADON, 'VI_PHAM_KE_TOAN: Cam tuyet doi xoa vat ly chung tu tai chinh. Vui long su dung chuc nang Huy Don!');
-END;
-/
