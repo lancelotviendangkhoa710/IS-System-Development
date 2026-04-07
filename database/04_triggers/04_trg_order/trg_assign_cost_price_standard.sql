6db0fed06b6846ad583c03c5264efd0166118b05
lancelotviendangkhoa710 <kvu6884@gmail.com>
2026-04-07 21:07:37 +0700

    fix: rename trigger

--- database/04_triggers/04_trg_order/trg_assign_cost_price_standard.sql
+++ database/04_triggers/04_trg_order/trg_assign_cost_price_standard.sql
@@ -1,5 +1,6 @@
    -- Trigger gán giá vốn tại thời điểm bán cho chi tiết đơn hàng
-    CREATE OR REPLACE TRIGGER TRG_CTDONHANG_GIAVON
+    CREATE OR REPLACE TRIGGER TRG_GAN_GIAVON_BAN
+
     BEFORE INSERT ON CTDONHANG
     FOR EACH ROW
     BEGIN
