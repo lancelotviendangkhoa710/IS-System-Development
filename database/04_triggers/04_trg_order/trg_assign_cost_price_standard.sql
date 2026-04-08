d46c3cffd2a2a1539e28922f347686540c018916
KhangdaVinci <duongdangkhang53@gmail.com>
2026-04-08 13:20:20 +0700

    keep diagram,report

--- database/04_triggers/04_trg_order/trg_assign_cost_price_standard.sql
+++
@@ -1,8 +0,0 @@
-   -- Trigger gán giá vốn tại thời điểm bán cho chi tiết đơn hàng
-    CREATE OR REPLACE TRIGGER TRG_CTDONHANG_GIAVON
-    BEFORE INSERT ON CTDONHANG
-    FOR EACH ROW
-    BEGIN
-        :NEW.DONGIAVON := FUNC_TinhGiaVonDong(:NEW.MASP);
-    END;
-    /
