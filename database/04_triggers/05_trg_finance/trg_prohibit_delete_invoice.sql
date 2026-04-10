7359e208ff90fecc9f78533aeb7f1f9f1a27db5b
hoagkhim <hkwknfil@gmail.com>
2026-04-10 18:34:38 +0700

    update: 05_trg_finance
    Vấn đề là 2 trigger đều tên TRG_CAMXOAKETOAN_HOADON dẫn đến Oracle sẽ tự ghi đè cái đầu bằng cái sau, kết quả là chỉ còn 1 trigger nhưng error code lại là của PHIEUTHUCHI, chắc là do nhầm lẫn gì đó tên tui sửa lại nge

--- database/04_triggers/05_trg_finance/trg_prohibit_delete_invoice.sql
+++ database/04_triggers/05_trg_finance/trg_prohibit_delete_invoice.sql
@@ -3,6 +3,6 @@ CREATE OR REPLACE TRIGGER TRG_CAMXOAKETOAN_HOADON
 BEFORE DELETE ON HOADON
 FOR EACH ROW
 BEGIN
-    RAISE_APPLICATION_ERROR(PKG_ERROR_CODES.ERR_TC_CAM_XOA_HOADON, 'VI_PHAM_KE_TOAN: Cam tuyet doi xoa vat ly chung tu tai chinh. Vui long su dung chuc nang Huy Don!');
+    RAISE_APPLICATION_ERROR(PKG_ERROR_CODES.ERR_TC_CAM_XOA_HOADON, 'Vi phạm kế toán: Cấm tuyệt đối xóa vật lý chứng từ hóa đơn.');
 END;
 /
